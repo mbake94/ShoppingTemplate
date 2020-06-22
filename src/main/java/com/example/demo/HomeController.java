91fa8a922f47e129a6c702b38dd22ed41ab65752
mbake94 <mbake94@gmail.com>
2020-06-22 10:07:04 -0700

    security config

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -22,39 +22,8 @@ public class HomeController {
 
     @RequestMapping("/")
     public String home(){
-        return "guestview";
+        return "userview";
     }
-
-
-
-
-        @RequestMapping("/login")
-        public String login(){
-            return "login";
-        }
-        @GetMapping("/signup")
-        public String signup(Model model){
-            model.addAttribute("user", new User());
-            return "signup";
-        }
-        @PostMapping("/processsignup")
-        public String processSignup(@Valid @ModelAttribute("user") User user, BindingResult result, Model model) {
-            model.addAttribute("user", user);
-            if (result.hasErrors()) {
-                return "signup";
-            } else {
-                model.addAttribute("message", "You have successfully created an account!");
-                user.setEnabled(true);
-                Role role = new Role(user.getUsername(), "ROLE_USER");
-                Set<Role> roles = new HashSet<Role>();
-                roles.add(role);
-
-                roleRepository.save(role);
-                userRepository.save(user);
-            }
-            return "redirect:/login";
-        }
-
 
         @RequestMapping("/logout")
         public String logout(){
--- src/main/java/com/example/demo/SecurityConfiguration.java
+++ src/main/java/com/example/demo/SecurityConfiguration.java
@@ -5,51 +5,29 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import javax.sql.DataSource;
 
-
 @Configuration
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 
-    @Override
-    protected void configure(HttpSecurity httpSecurity) throws Exception{
-        httpSecurity.authorizeRequests()
-                .antMatchers("/signup", "/h2-console/**", "/processsignup").permitAll()
-                .antMatchers("/admin").hasRole("ADMIN")
-                .antMatchers("/register").hasRole("ADMIN")
-                .antMatchers("/**").hasAnyRole( "ADMIN", "USER")
-//                .anyRequest().authenticated()
-                .and()
-                .formLogin()
-                .loginPage("/login").permitAll()
-                .and()
-                .logout()
-                .logoutSuccessUrl("/login?logout=true").permitAll();
-
-        httpSecurity.csrf().ignoringAntMatchers("/h2-console/**");
-        httpSecurity.headers().frameOptions().sameOrigin();
-
-    }
-
-
-
-    @Autowired
-    DataSource dataSource;
-
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception{
-        auth.jdbcAuthentication()
-                .dataSource(dataSource)
-                .usersByUsernameQuery("select username, password, enabled from " + "user_db where username=?")
-                .authoritiesByUsernameQuery("select username, role from roles " + "where username =?");
-    }
     @Bean
-    public PasswordEncoder passwordEncoder() {
-
+    public static BCryptPasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http
+                .authorizeRequests().anyRequest().authenticated().and().formLogin().loginPage("/login").permitAll();
+    }
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+        auth.inMemoryAuthentication().withUser("user").password(passwordEncoder().encode("user")).authorities("USER");
+        auth.inMemoryAuthentication().withUser("admin").password(passwordEncoder().encode("admin")).authorities("ADMIN");
     }
 }
